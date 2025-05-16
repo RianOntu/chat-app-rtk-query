0d56dace621fa028560737f4a0db1fad48eddcda
RianOntu <rianontu13@gmail.com>
2025-05-16 22:29:23 +0600

    feat:created apiSlice

---
+++ src/features/api/apiSlice.js
@@ -0,0 +1,19 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+const apiSlice = createApi({
+  reducerPath: "api",
+  baseQuery: fetchBaseQuery({
+    baseUrl: process.env.REACT_APP_URL,
+    prepareHeaders: async (header, { getState, endpoints }) => {
+      const token = getState().auth.accessToken;
+      if (token) {
+        header.set(`Authorization`, `Bearer ${token}`);
+      }
+      return header;
+    },
+  }),
+  tagTypes: [],
+  endpoints: (builder) => ({}),
+});
+
+export default apiSlice;
